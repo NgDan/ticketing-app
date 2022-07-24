b1ea1e56c345a5fea7831eadcba9be1335dca055
Dan Negrusa <negrusadan95@gmail.com>
2022-07-24 19:45:38 +0100

    added error subclasses, handle different error types in error-handler, map erorrs into standard format

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 // we'll use this handler throughout all our microservies
 // so it catches all the errors and normalizez them with
@@ -11,7 +13,30 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    console.log('handling this error as a request validation error');
+
+    // here we are mapping the errors with the RequestValidationError type
+    // into our standard error format which is the following:
+    // {
+    //  errors: {
+    //     message: string,
+    //     field?: string
+    //  }[]
+    // }
+
+    const formattedErrors = err.errors.map(error => ({
+      message: error.msg,
+      field: error.param,
+    }));
+
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    // mapping into our standard error format
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
   const { message } = err;
-  res.status(400).send({ message });
+  // mapping into our standard error format
+  res.status(400).send({ errors: [{ message: 'Something went wrong' }] });
 };
