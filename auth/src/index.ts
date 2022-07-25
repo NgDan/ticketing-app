16725e271261670c05ccc80d8263805b83c439b0
Dan Negrusa <negrusadan95@gmail.com>
2022-07-25 20:58:59 +0100

    handle async errors with throw syntax in express, add not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+
+// express-async-errors lets us use the throw keyword in async handlers
+// by default, express can't handle throw statements inside async functions,
+// we would need to use the next function and pass the error there instead of
+// throwing it
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -6,6 +12,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -18,6 +25,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// app.app is a combination of app.get, app.post, app.delete, etc
+// it basically responds to any request method
+app.all('*', async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
