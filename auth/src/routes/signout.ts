5eb702c1e75505e74cb2d4bafb0001514c66bf68
Dan Negrusa <negrusadan95@gmail.com>
2022-08-05 20:33:55 +0100

    add signout route handler

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there!');
+  // cookie-parser will attach a delete cookie header
+  // to the response if we set it to null
+
+  req.session = null;
+  res.send({});
 });
 
 // we're renaming it as we export it
