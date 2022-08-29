54f12d47a885cac298de9fa39dee794fd9fbf3cf
Dan Negrusa <negrusadan95@gmail.com>
2022-08-29 14:33:14 +0100

    generate valid format id for test

--- tickets/src/routes/test/show.test.ts
+++ tickets/src/routes/test/show.test.ts
@@ -1,13 +1,18 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { createSessionCookie } from '../../test/setup';
+
 it('returns a 404 if the ticket is not found', async () => {
-  await request(app).get('/api/tickets/random-test-id').send().expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 it('returns a ticket if the ticket is found', async () => {
   const title = 'Test title';
   const price = 20;
   // create ticket
+
   const response = await request(app)
     .post('/api/tickets')
     .set('Cookie', createSessionCookie())
