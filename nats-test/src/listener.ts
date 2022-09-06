0e24d411c3da32e1501504d5d99b74c40f4775c4
Dan Negrusa <negrusadan95@gmail.com>
2022-09-06 22:21:11 +0100

    amend comment

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -17,7 +17,7 @@ client.on('connect', () => {
     // when we have multiple instances of the same service running
     // in parallel for scaling purposes but we don't want the
     // event sent to all the instances because that would result
-    // in duplicate work. For example, a ticket service that saves
+    // in duplicate processing. For example, a ticket service that saves
     // tickets in a db as a response to an event would duplicate
     // tickets saved in the db if all the instances were to process
     // the same event
