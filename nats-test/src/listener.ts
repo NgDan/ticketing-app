d2604c47671184555f0c5d0304294f762ad8eb82
Dan Negrusa <negrusadan95@gmail.com>
2022-09-13 19:26:17 +0100

    add TicketCreatedListener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -13,53 +13,8 @@ client.on('connect', () => {
     process.exit();
   });
 
+  new TicketCreatedListener(client).listen();
   console.log('listener connected to nats');
-
-  // Ack stands for acknowledgement. This enables us to manually confirm we've
-  // processed an event so the event bus can dispose it. Without manual
-  // confirmation (acknowledgement), we risk losing important data if
-  // some event isn't processed due to some failure (db failure, error in our
-  // own code, etc). If the event bus doesn't receive an acknowledgement for
-  // an event, it's going to wait a certain amount of time and re-emit that
-  // event again so we have another chance at processing it.
-  const options = client
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    // this option enables NATS to save a copy of each event emitted
-    // for a queue group and store it internally. If the service that
-    // processes that event acknowledges it, it will mark that event
-    // as "processed" internally. If a service goes down, and restarts
-    // at some point in the future, NATS then will re-emit all the events
-    // that haven't been marked as "processed" to the service so it has
-    // a chance to process them again.
-    .setDurableName('accounting-service');
-
-  const subscription = client.subscribe(
-    'ticket:created',
-    // queue groups tell the streaming service to only send one
-    // event to the members of that queue group. This is helpful
-    // when we have multiple instances of the same service running
-    // in parallel for scaling purposes but we don't want the
-    // event sent to all the instances because that would result
-    // in duplicate processing. For example, a ticket service that saves
-    // tickets in a db as a response to an event would duplicate
-    // tickets saved in the db if all the instances were to process
-    // the same event
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-    // getData() returns either a string or a buffer
-    if (typeof data === 'string') {
-      console.log('message sequence', msg.getSequence());
-      console.log('message data', JSON.parse(data));
-    }
-    // confirm we've finished processing the event (happy path)
-    msg.ack();
-  });
 });
 
 // these callbacks are fired right before we kill our process
@@ -71,6 +26,15 @@ process.on('SIGTERM', () => client.close());
 
 abstract class Listener {
   abstract subject: string;
+  // queue groups tell the streaming service to only send one
+  // event to the members of that queue group. This is helpful
+  // when we have multiple instances of the same service running
+  // in parallel for scaling purposes but we don't want the
+  // event sent to all the instances because that would result
+  // in duplicate processing. For example, a ticket service that saves
+  // tickets in a db as a response to an event would duplicate
+  // tickets saved in the db if all the instances were to process
+  // the same event
   abstract queueGroupName: string;
   private client: Stan;
   protected ackWait = 5 * 1000;
@@ -81,12 +45,28 @@ abstract class Listener {
   }
 
   subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
+    return (
+      this.client
+        .subscriptionOptions()
+        .setDeliverAllAvailable()
+        .setManualAckMode(true)
+        // Ack stands for acknowledgement. This enables us to manually confirm we've
+        // processed an event so the event bus can dispose it. Without manual
+        // confirmation (acknowledgement), we risk losing important data if
+        // some event isn't processed due to some failure (db failure, error in our
+        // own code, etc). If the event bus doesn't receive an acknowledgement for
+        // an event, it's going to wait a certain amount of time and re-emit that
+        // event again so we have another chance at processing it.
+        .setAckWait(this.ackWait)
+        // this option enables NATS to save a copy of each event emitted
+        // for a queue group and store it internally. If the service that
+        // processes that event acknowledges it, it will mark that event
+        // as "processed" internally. If a service goes down, and restarts
+        // at some point in the future, NATS then will re-emit all the events
+        // that haven't been marked as "processed" to the service so it has
+        // a chance to process them again.
+        .setDurableName(this.queueGroupName)
+    );
   }
 
   listen() {
@@ -108,5 +88,17 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data: ', data);
+
+    // after we succesfully process the message
+    msg.ack();
   }
 }
