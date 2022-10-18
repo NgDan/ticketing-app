53eb6a715b06668de726e5069f2dd0de20a87bb9
Dan Negrusa <negrusadan95@gmail.com>
2022-10-18 18:55:08 +0100

    add orders model and schema

---
+++ orders/src/models/order.ts
@@ -0,0 +1,57 @@
+import mongoose, { mongo } from 'mongoose';
+
+// we have 2 different interfaces (OrderAttrs and OrderDoc) because the attributes
+// that will end up on an Order are different from the ones needed to create an Order
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
