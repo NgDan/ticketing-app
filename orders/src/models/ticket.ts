97cce9a33331ef39a23d4f617f75d5cfac2301d8
Dan Negrusa <negrusadan95@gmail.com>
2022-10-20 16:20:48 +0100

    complete POST order route with ticket checking and addfing logic

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
 
 // this model SHOULD NOT be identical to the one in tickets models.
 // Our service, even if it needs to duplicate the tickets data in its own
@@ -13,6 +14,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -42,6 +44,25 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => new Ticket(attrs);
+
+// important! this should not be an arrow function
+ticketSchema.methods.isReserved = async function () {
+  // run query to look at all orders. Find order where the ticket matches the one this
+  // request is interested in *and* the order status is *not* cancelled.
+  // If we find an order from that means the ticket *is* reserved.
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+  // return just a boolean not the whole order
+  return !!existingOrder;
+};
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@ng-ticketing-app/common';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@ng-ticketing-app/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
 
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -25,17 +32,40 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
     // find the ticket the user is trying to order in the database (if we fail to fetch the ticket from the db it means it's already been sold, deleted, etc)
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
 
     // make sure this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
 
     // calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // build the order and save it to the database
+    const order = Order.build({
+      // we're certain the currentUser property is defined since we've got the requireAuth middleware
+      // that would've bailed out if the currentUser wasn't there
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
 
     // publish an event to notify an order has been created
 
-    res.send({});
+    res.send(201).send(order);
   }
 );
 
