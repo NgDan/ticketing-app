458b6bcfc806218301f7db77357a952e9dd8e638
Dan Negrusa <negrusadan95@gmail.com>
2022-10-21 19:17:17 +0100

    add delete route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,35 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@ng-ticketing-app/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { OrderStatus } from '@ng-ticketing-app/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    // if we followed proper REST architecture this DELETE method would've
+    // actually deleted the record instead of just marking it as Cancelled
+    order.status = OrderStatus.Cancelled;
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
