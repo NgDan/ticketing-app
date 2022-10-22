6b2724cde2b8944ad68ff29874c504059dc1afb7
Dan Negrusa <negrusadan95@gmail.com>
2022-10-22 17:09:14 +0100

    raise order cancelled event

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,8 @@ import {
 import express, { Request, Response } from 'express';
 import { Order } from '../models/order';
 import { OrderStatus } from '@ng-ticketing-app/common';
+import { OrderCancelledPublisher } from '../events/publisher/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -15,7 +17,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -30,6 +32,12 @@ router.delete(
     await order.save();
 
     // publish an event to notify this was cancelled
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
