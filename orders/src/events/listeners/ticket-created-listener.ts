6735b8558d9bf1b6e70c6ac2195fa58eabe9fad0
Dan Negrusa <negrusadan95@gmail.com>
2022-10-22 17:41:12 +0100

    extract queuegroupname to a constant

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -5,10 +5,12 @@ import {
   TicketCreatedEvent,
 } from '@ng-ticketing-app/common';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
+  // this notation (TicketCreatedEvent['data']) is used to get the type of the data attribute on the TicketCreatedEvent interface
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
