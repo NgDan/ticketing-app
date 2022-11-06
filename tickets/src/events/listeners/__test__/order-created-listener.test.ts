03b0cfb47d072f35f83e2507528f32e322e877c4
Dan Negrusa <negrusadan95@gmail.com>
2022-11-06 15:54:57 +0000

    add test setup function

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,39 @@
+import { OrderCreatedEvent, OrderStatus } from '@ng-ticketing-app/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created-listener';
+
+const setup = async () => {
+  // create an instance of a listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 99,
+    userId: 'asdfgadfgdag',
+  });
+  await ticket.save();
+
+  // create the fake data object
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: 'sdafasdfgads',
+    expiresAt: 'fsfsafsadf',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -5,7 +5,7 @@ interface TicketAttrs {
   title: string;
   price: number;
   userId: string;
-  orderId: string;
+  orderId?: string;
 }
 
 interface TicketDoc extends mongoose.Document {
