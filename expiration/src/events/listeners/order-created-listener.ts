a5c59a26cda588355d0bdc851776199d7bbd5a2f
Dan Negrusa <negrusadan95@gmail.com>
2022-11-12 13:20:04 +0000

    add ortder created listener, move files to events folder

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,11 +5,18 @@ import {
   Subjects,
 } from '@ng-ticketing-app/common';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 import { queueGroupName } from './queueGroupName';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
 }
