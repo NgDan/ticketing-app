b1b6679ee417cb49ef8e054d9b826a1b5fc3f8b6
Dan Negrusa <negrusadan95@gmail.com>
2022-11-15 19:26:36 +0000

    add updateIfCurrentPlugin to order schema

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,5 +1,6 @@
 import { OrderStatus } from '@ng-ticketing-app/common';
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 // the payments service will replicate its own table
 // with orders with the relevant attributes
@@ -46,6 +47,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
