b9d23dad6b1f51eaaea9f0ee37f4b17a93743f6b
Dan Negrusa <negrusadan95@gmail.com>
2023-05-10 18:27:25 +0100

    add stripe charges unit test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,7 +4,11 @@ import { app } from '../../app';
 import { createSessionCookie } from '../../test/setup';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@ng-ticketing-app/common';
+import { stripe } from '../../stripe';
 
+jest.mock('../../stripe');
+
+const ticketPrice = 20;
 it('should throw a 404 when purchasing an order that does not exist', async () => {
   const user = createSessionCookie();
   await request(app)
@@ -21,7 +25,7 @@ it('should throw a 401 when purchasing an order that does not belong to the user
     id: new mongoose.Types.ObjectId().toHexString(),
     userId: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
-    price: 20,
+    price: ticketPrice,
     status: OrderStatus.Created,
   });
   await order.save();
@@ -43,7 +47,7 @@ it('should throw a 400 when purchasing a cancelled order', async () => {
     id: new mongoose.Types.ObjectId().toHexString(),
     userId,
     version: 0,
-    price: 20,
+    price: ticketPrice,
     status: OrderStatus.Cancelled,
   });
   await order.save();
@@ -58,4 +62,31 @@ it('should throw a 400 when purchasing a cancelled order', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: ticketPrice,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', createSessionCookie(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.amount).toEqual(ticketPrice * 100);
+  expect(chargeOptions.currency).toEqual('usd');
 });
