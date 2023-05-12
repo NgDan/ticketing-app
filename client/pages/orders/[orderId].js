36970bc4bcbbe9221d25101a40512c5503b18737
Dan Negrusa <negrusadan95@gmail.com>
2023-05-12 18:36:26 +0100

    replace stripeKey (public) with my own key

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
-import StripeCheckout from "react-stripe-checkout";
-import Router from "next/router";
-import useRequest from "../../hooks/use-request";
+import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
   const { doRequest, errors } = useRequest({
-    url: "/api/payments",
-    method: "post",
+    url: '/api/payments',
+    method: 'post',
     body: {
       orderId: order.id,
     },
-    onSuccess: () => Router.push("/orders"),
+    onSuccess: () => Router.push('/orders'),
   });
 
   useEffect(() => {
@@ -37,7 +37,7 @@ const OrderShow = ({ order, currentUser }) => {
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
         token={({ id }) => doRequest({ token: id })}
-        stripeKey="pk_test_JMdyKVvf8EGTB0Fl28GsN7YY"
+        stripeKey="pk_test_51N5x7bDgXUYi9QirTfX5vPJcx8YcTz8QHOT7kolRvWKqNxHcOYEtlaXELte2sg0O74q16iPsS7JGL3o26br8byqA00JaurZpwN"
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
